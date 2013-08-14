8a0da13205915c744ccd7d47e89d9102ac26a10f
Andrey Sibiryov <me@kobology.ru>
2013-08-14 17:02:51 +0400

    Barely working FUSE-based crap.

---
+++ cocainefs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import cocaine.services, routes, routefs
+
+class CocaineFS(routefs.RouteFS):
+    def __init__(self, *args, **kwargs):
+        super(CocaineFS, self).__init__(*args, **kwargs)
+
+        # Initialize the Storage Service connection
+        self.storage = cocaine.services.Service('storage')
+
+    def make_map(self):
+        result = routes.Mapper()
+
+        result.connect('/', controller = 'namespaces')
+        result.connect('/{namespace}', controller = 'keys')
+        result.connect('/{namespace}/{key}', controller = 'read')
+
+        return result
+
+    def namespaces(self, **kwargs):
+        return list(self.storage.find('system', ['public', 'namespace']))
+
+    def keys(self, namespace, **kwargs):
+        return list(self.storage.find(namespace, ['public']))
+
+    def read(self, namespace, key, **kwargs):
+        return str(self.storage.read(namespace, key).get())
+
+if __name__ == "__main__":
+    routefs.main(CocaineFS)
